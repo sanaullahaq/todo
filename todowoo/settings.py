7d39a5cb51fa4af26f3d5d84c53126fb2c9af81f
sanaullahaq <sanaullahaq1997@gmail.com>
2020-08-03 09:01:58 +0600

    -

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
 
 LOGIN_URL = '/login'
 # if a user wants to create,view current todo, delete etc with out login this will redirect them to login first,
