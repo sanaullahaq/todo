29da9bcd19f97c990946912897fed6f1cd71f61d
sanaullahaq <sanaullahaq1997@gmail.com>
2020-08-07 15:18:01 +0600

    few spelling mistaked removed

--- todo/admin.py
+++ todo/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Todo
 
 
-# since in our Todo Model we have a filed call created what is not editable that's why django does not show in the
-#  admin panel but we just want to see it only that is why the below class created what is all most a constant with
+# since in our Todo Model we have a filed called created what is not editable that's why django does not show in the
+#  admin panel but we just want to see it only, that is why the below class created what is all most a constant with
 #  the field name inside the parenthesis
 class TodoAdmin(admin.ModelAdmin):
     readonly_fields = ('created',)
